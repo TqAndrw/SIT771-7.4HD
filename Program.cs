6bc1f211e1c580eff56986707ea0809b7891b33f
Andrew Tran <tqtoan5703@gmail.com>
2026-09-14 21:23:33 +1000

    Add game entry point

---
+++ Program.cs
@@ -0,0 +1,34 @@
+using SplashKitSDK;
+
+namespace RobotDodge
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Window gameWindow =
+                new Window(
+                    "Robot Dodge",
+                    800,
+                    600
+                );
+
+            RobotDodge game =
+                new RobotDodge(
+                    gameWindow
+                );
+
+            while (!game.Quit &&
+                   !gameWindow.CloseRequested)
+            {
+                SplashKit.ProcessEvents();
+
+                game.HandleInput();
+                game.Update();
+                game.Draw();
+            }
+
+            gameWindow.Close();
+        }
+    }
+}
